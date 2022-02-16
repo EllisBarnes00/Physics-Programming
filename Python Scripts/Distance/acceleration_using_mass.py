4a0b9f3a303a70eb31af166510a7e026ca1248ff
Ellis Barnes <ellisbarnes00@gmail.com>
2022-02-16 16:39:52 -0500

    Added accleration using mass

---
+++ Python Scripts/Distance/acceleration_using_mass.py
@@ -0,0 +1,8 @@
+# Acceleration can be determined by weight 
+# as well using a = (Net Force / mass)
+
+net_force = float(input("Net Force: "))
+mass = float(input("Mass: "))
+acceleration = (net_force/mass)
+
+print("Acceration: ", acceleration)
