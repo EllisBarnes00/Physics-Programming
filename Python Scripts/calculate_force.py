cdd0987e837aafa129f81f0b59e0debc65526235
Ellis Barnes <ellisbarnes00@gmail.com>
2022-02-16 19:26:42 -0500

    Added calculation for force

---
+++ Python Scripts/calculate_force.py
@@ -0,0 +1,10 @@
+# The force of something is mass * acceleration 
+# and is measured in Newtons
+# F = m*a
+# 316.16N = 32kg * g (where g=9.88) 
+
+mass = float(input("Mass: "))
+acceleration = float(input("Acceleration: "))
+force = mass * acceleration 
+
+print("Force: ", force, "\bN")
