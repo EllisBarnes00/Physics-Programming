2d70717d1909ffe69c416f1e3d370dca5dfe962b
Ellis Barnes <ellisbarnes00@gmail.com>
2022-04-16 16:05:06 -0400

    Calcuate wattage script

---
+++ Python Scripts/Electrostatic/calculate_wattage.py
@@ -0,0 +1,9 @@
+# Current is the flow of charges
+# Voltage is the electric potential
+
+current = float(input("Current/Amps: "))
+voltage = float(input("Voltage: "))
+
+power = current * voltage
+
+print("Watts/Power: {0} Watts".format(power))
