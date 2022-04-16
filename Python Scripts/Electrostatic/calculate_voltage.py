71cd3cda6dfb34b61195eaa1244d8a7281e3b3fe
Ellis Barnes <ellisbarnes00@gmail.com>
2022-04-16 15:59:49 -0400

    Calcuate voltage script

---
+++ Python Scripts/Electrostatic/calculate_voltage.py
@@ -0,0 +1,14 @@
+# Voltage is the electric potential
+
+# Electric Potential Energy: Energy possessed by a charged particle due to its 
+# location in an electric field. Work is required to push a 
+# charged particle against the electric field of a charged 
+# body.
+
+
+electric_potential_energy = float(input("Electric Potential Energy: "))
+amount_of_charge = int(input("Charge amount: "))
+
+voltage = electric_potential_energy/amount_of_charge
+
+print(f"Voltage/Electric Potential: {voltage} V")
